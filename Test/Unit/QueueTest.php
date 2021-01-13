5add4267e2d22a0f4aaba0c487205fbef1ca4134
Andreus Timm <andreus.timm@gmail.com>
2021-01-13 10:08:01 -0300

    Magento 2 Module - Version 1.0.9

---
+++ Test/Unit/QueueTest.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types=1);
+
+namespace Mahout\ERP\Test\Unit;
+
+class QueueTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Is called once before running all test in class
+     */
+    public static function setUpBeforeClass()
+    {
+        //setup
+    }
+
+    /**
+     * Is called once after running all test in class
+     */
+    public static function tearDownAfterClass()
+    {
+        //teardown
+    }
+
+    /**
+     * Is called before running a test
+     */
+    protected function setUp()
+    {
+        //setup
+    }
+
+    /**
+     * Is called after running a test
+     */
+    protected function tearDown()
+    {
+        //teardown
+    }
+
+    /**
+     * The test itself, every test function must start with 'test'
+     */
+    public function testGet()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * The test itself, every test function must start with 'test'
+     */
+    public function testList()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * The test itself, every test function must start with 'test'
+     */
+    public function testCreate()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * The test itself, every test function must start with 'test'
+     */
+    public function testUpdate()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * The test itself, every test function must start with 'test'
+     */
+    public function testDelete()
+    {
+        $this->assertTrue(false);
+    }
+}
+
